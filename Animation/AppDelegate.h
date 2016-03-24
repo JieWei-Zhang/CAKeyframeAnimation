7f3225a9c22d85174b60afa0236d713db83bcf71
AndersonLeeK <836654084@qq.com>
2016-03-24 09:33:57 +0800

    Initial Commit

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by Vinhome on 16/3/24.
+//  Copyright © 2016年 Vinhome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
